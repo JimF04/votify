7491775447f4229649383dbf2171c5821eeb730b
jose <j.campos@estudiantec.cr>
2024-02-20 15:17:30 -0600

    avances ventanita

--- cliente/src/main.rs
+++ cliente/src/main.rs
@@ -1,12 +1,26 @@
-fn main() {
-    MainWindow::new().unwrap().run().unwrap();
-}
+
 
 slint::slint! {
-    export component MainWindow inherits Window {
-        Text {
-            text: "hello world";
-            color: green;
+    import { Button, VerticalBox } from "std-widgets.slint";
+    component MainWindow inherits Window {
+
+        VerticalBox {
+            alignment: start;
+            Text {text: "hello uwu";}
+            Button {text: "uwu";}
+            Rectangle {
+                width: 100px;
+                height: 100px;
+                background:  #3960D5;
+            }
+
+
         }
+   
     }
+    
+}
+
+fn main() {
+    MainWindow::new().unwrap().run().unwrap();
 }
