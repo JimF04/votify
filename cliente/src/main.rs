ce9a3948e2a2cce58ee36a9c404ad4879d316ce8
jose <j.campos@estudiantec.cr>
2024-02-21 22:15:49 -0600

    avances con la ventanita, propablemente cambie

--- cliente/src/main.rs
+++ cliente/src/main.rs
@@ -1,26 +1,58 @@
-
+static window_size: i32 = 800;
 
 slint::slint! {
-    import { Button, VerticalBox } from "std-widgets.slint";
-    component MainWindow inherits Window {
+    import { Button, VerticalBox, HorizontalBox } from "std-widgets.slint";
 
-        VerticalBox {
-            alignment: start;
-            Text {text: "hello uwu";}
-            Button {text: "uwu";}
+    component MainWindow inherits Window {
+        width: 800px;
+        title: "VOTIFY";
+
+        VerticalLayout {
+
+
+
+            HorizontalLayout {
+                Text {
+                text: "cancion";
+            }
+
             Rectangle {
-                width: 100px;
-                height: 100px;
-                background:  #3960D5;
+                width: 400px;
+                height: 50px;
+                background: red;
             }
 
 
+            }
+       
         }
-   
+
+        VerticalLayout {
+
+            HorizontalLayout{
+                Text {
+                text: "Cancion1";
+            }
+
+            Rectangle {
+                width: 400px;
+                height: 50px;
+                background: blue;
+            }
+
+            padding-top: 50px;
+
+        }
+
+
+            
+
+            
+        }
     }
-    
 }
 
 fn main() {
     MainWindow::new().unwrap().run().unwrap();
+
 }
