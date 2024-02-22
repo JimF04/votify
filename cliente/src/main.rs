f49684138b9454412ab366d7772b97f47fe32b88
jose <j.campos@estudiantec.cr>
2024-02-21 22:30:51 -0600

    cree el componente de cancion

--- cliente/src/main.rs
+++ cliente/src/main.rs
@@ -1,54 +1,49 @@
 static window_size: i32 = 800;
 
+fn crear_cancion(){
+
+
+
+
+
+}
+
 slint::slint! {
     import { Button, VerticalBox, HorizontalBox } from "std-widgets.slint";
+
+
+
+    component Cancion inherits VerticalLayout {
+
+            HorizontalLayout {
+            Text {
+        text: "cancion";
+                }
+
+            Rectangle {
+        width: 400px;
+        height: 50px;
+        background: red;
+                }
+
+
+}
+
+}
 
     component MainWindow inherits Window {
         width: 800px;
         title: "VOTIFY";
-
-        VerticalLayout {
-
+        background: #FB88B4;
 
 
-            HorizontalLayout {
-                Text {
-                text: "cancion";
-            }
-
-            Rectangle {
-                width: 400px;
-                height: 50px;
-                background: red;
-            }
 
 
-            }
-       
-        }
 
-        VerticalLayout {
-
-            HorizontalLayout{
-                Text {
-                text: "Cancion1";
-            }
-
-            Rectangle {
-                width: 400px;
-                height: 50px;
-                background: blue;
-            }
-
-            padding-top: 50px;
-
-        }
+        Cancion{}
 
 
-            
 
-            
-        }
     }
 }
 
