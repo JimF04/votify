87839341a4a9356e79ed40406455debe41d3b7bc
gabrll05 <142859449+gabrll05@users.noreply.github.com>
2024-03-02 15:22:25 -0600

    Libreria

---
+++ cmake-build-debug/CMakeFiles/votify_gui.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for votify_gui.
+# This may be replaced when dependencies are built.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ahenao/Desktop/Proyecto Playlist Comunitaria/votify
+# Install script for directory: /home/gabrielvb/VotifyClone/votify
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ahenao/Desktop/Proyecto Playlist Comunitaria/votify/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/gabrielvb/VotifyClone/votify/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
