b934c4280009c923b338c2b573f69bfad9d4665d
gabrll05 <142859449+gabrll05@users.noreply.github.com>
2024-03-08 18:39:45 -0600

    Lista Empaginada

--- paginated_list/main.cpp
+++ paginated_list/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 #include <vector>
 
+#include "PaginatedList.cpp"
+
 int main(){
 
 
-    PaginatedList = PaginatedList
+    PaginatedList PaginatedList(3);
+
+    for(int i =0;i<=10;i++) {
+        PaginatedList.insert(i);
+    }
+
+
+    PaginatedList.printCurrentPage();
+
+
+    PaginatedList.nextPage();
+    PaginatedList.printCurrentPage();
+
+
+    PaginatedList.previousPage();
+    PaginatedList.printCurrentPage();
+
+
+
+    return 0;
+
 }
+
+
+
 
