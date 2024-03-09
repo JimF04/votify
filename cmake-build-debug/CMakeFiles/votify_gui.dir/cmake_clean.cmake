347a5a597c1e7ea2649eef300af3d27eaaa1171d
Alejandro Henao <ahenao@Ubuntu.myguest.virtualbox.org>
2024-03-08 18:22:12 -0600

    [FIX]: Build error

---
+++ cmake-build-debug/CMakeFiles/votify_gui.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/votify_gui.dir/ServerGui.cpp.o"
+  "CMakeFiles/votify_gui.dir/ServerGui.cpp.o.d"
+  "votify_gui"
+  "votify_gui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/votify_gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
